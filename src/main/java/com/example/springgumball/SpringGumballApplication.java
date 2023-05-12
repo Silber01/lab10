1cd44964b093618a2772cb169b209c9d4bdbba2d
Daniel Slade <dangslade@gmail.com>
2023-05-11 22:26:00 -0700

    test commit 2

--- src/main/java/com/example/springgumball/SpringGumballApplication.java
+++ src/main/java/com/example/springgumball/SpringGumballApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringGumballApplication {
-
+	//this is a test commit
 	public static void main(String[] args) {
 		SpringApplication.run(SpringGumballApplication.class, args);
 	}
